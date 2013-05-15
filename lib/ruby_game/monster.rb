e0f254d0adb97bfac8dda72f5b21daeae1447d8d
DrBenton <olivier@dr-benton.com>
2013-05-15 15:09:54 +0200

    new possibility to add actions to Monsters profiles

--- lib/ruby_game/monster.rb
+++ lib/ruby_game/monster.rb
@@ -4,21 +4,32 @@ module RubyGame
   
   class Monster < MovingObject
     
+    ACTION = Struct.new(:type, :speed)
+    
+    attr_reader :pending_actions
+    
     @@profiles = {}
     
     def initialize(x = nil, y = nil, image_name = 'ghost1')
       super
+      @pending_actions = []
+    end
+
+    def hello_world
+      puts "New monster on the bridge, captain! My object id is #{object_id}, I have #{@pending_actions.length} pending actions to handle"
     end
 
     def self.define_profile(profile_name_sym, &init_block)
-      monster_profile_model = Monster.new
-      monster_profile_model.instance_eval(&init_block)
-      @@profiles[profile_name_sym] = monster_profile_model
+      @@profiles[profile_name_sym] = init_block
     end
 
     def self.build(profile_name_sym)
-      monster_profile_model = @@profiles[profile_name_sym]
-      monster_profile_model.clone
+      monster_profile_init_block = @@profiles[profile_name_sym]
+      puts "monster_profile_model.pending_actions = "
+      new_monster = self.new
+      new_monster.instance_eval(&monster_profile_init_block)
+      new_monster.hello_world
+      new_monster
     end
 
     def init_limits(max_width, max_height, border_width, border_top_width)
@@ -35,7 +46,30 @@ module RubyGame
       @to_target_speed = speed
     end
     
+    def action(type, opts = nil)
+      puts "#{self.class}.action(#{type}, #{opts.inspect})"
+      # opts setup
+      opts ||= {}
+      speed = opts[:speed] || DEFAULT_SPEED
+      nb_actions = opts[:repeat] || 1
+      # go!
+      @pending_actions ||= []
+      nb_actions.times do
+        @pending_actions << ACTION.new( type, speed ) 
+      end
+    end
+    
     def update
+      
+      #puts @pending_actions.length
+      
+      unless @pending_actions.empty?
+        next_action = @pending_actions.shift
+        #puts "next_action=#{next_action.inspect}"
+        send("#{next_action.type}", next_action.speed)
+        super
+        return
+      end
       
       unless @target.nil?
         
